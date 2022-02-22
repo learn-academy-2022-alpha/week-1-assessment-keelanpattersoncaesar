b88eeadf3bfd51ecd15be6f21f652d073a3bce9c
keelanpattersoncaesar <keelanpattersoncaesar@gmail.com>
2022-02-22 09:43:44 -0800

    completed assessment

--- code-challenges.js
+++ code-challenges.js
@@ -1,49 +1,90 @@
-// ASSESSMENT 1: Coding Practical Questions
+// // ASSESSMENT 1: Coding Practical Questions
+//
+// // To run file: navigate to the correct directory in terminal, $ node code-challenges.js
+//
+// // Pseudo coding is required.
+//
+// // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in any two strings and returns the string with more characters. Use the two sets of test variables provided.
+//
+// // Set one:
+// const fruit1 = "apple"
+// const fruit2 = "banana"
+//
+// // Set two:
+// const fruit3 = "cherry"
+// const fruit4 = "kiwi"
+//
+//
+// // --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+//
+// //Pseudo code: create if statement
+// // if 42 or lower = below
+// // if exactly 212 = Boiling
+// // else 350 or above = above
+//
+// // const temp1 = 42
+// // const temp2 = 350
+// // const temp3 = 212
+// //
+// // if (temp1 <= 211) {
+// //   console.log("42 is below boiling point")
+// // } elseif (temp2 >= 349); {
+// //   console.log("350 is above boiling point")
+// // } else; {
+// //   console.log("212 is at boiling point")
+// // }
+//
+//
+// // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
+//
+// const myNumbers1 = [3, 7, 0, 36, -9]
+// const myNumbers2 = [8, -7, 42, 9, 13]
+//
+// // psuedo code: declare both variables (done), concat myNumbers1 into myNumbers2, execute .length into console.log()
+//
+// const myNumbers3 = myNumbers1.concat(myNumbers2)
+// console.log(myNumbers3)
+//
+// // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2202 ahplA"
+//
+// psuedo code: Start with "Alpha 2022", console.log the string with the reverse tag, profit?
 
-// To run file: navigate to the correct directory in terminal, $ node code-challenges.js
+// const currentCohort = "Alpha 2022"
+// return currentCohort.reverse
+// console.log(currentCohort.reverse())
 
-// Pseudo coding is required.
+// // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in any two strings and returns the string with more characters. Use the two sets of test variables provided.
+// psuedo code: create if statement, check values against %2, === 0 means, it's even, else odd
 
-// Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
-
-// Set two:
-const fruit3 = "cherry"
-const fruit4 = "kiwi"
-
-
-// --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
-
-const temp1 = 42
-const temp2 = 350
-const temp3 = 212
+// const myArray = [13, 34, 5, 10, 27, 42]
+// if (myArray.index % 2 != 0) {
+//   console.log("The number is odd.")
+// } else {
+//   console.log("The number is even.")
+// }
 
 
-// --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
-
-const myNumbers1 = [3, 7, 0, 36, -9]
-const myNumbers2 = [8, -7, 42, 9, 13]
-
-
-// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
-
-const currentCohort = "Alpha 2022"
-
-
-// --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
-
-const myArray = [13, 34, 5, 10, 27, 42]
-
-
-// --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
-
+// // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
+//
+// psuedo code: declare function, assign value to the function so they can be called upon, return the value of the bigger - smaller, console.log
+//
 // Set one:
 const number1 = 58
 const number2 = 100
 
+let b = subtractionFunction(number2,number1)
+function subtractionFunction(x,y) {
+  return number2 - number1
+}
+console.log(b)
+
 // Set two:
 const number3 = 27
 const number4 = 24
+//
+let c = subtractionFunction2(number3,number4)
+function subtractionFunction2(x,y) {
+  return number3 - number4
+}
+console.log(c)
--- interview-questions.md
+++ interview-questions.md
@@ -15,41 +15,40 @@ INSTRUCTOR EXAMPLE: What is a conditional statement?
 
 1. What is git? What is the difference between git and Github?
 
-  Your answer:
+  Your answer: Git is a version control software. It lets multiple developers work on projects without putting said projects in jeopardy. Github is just a way to manage your project's "code history" in the cloud.
 
-  Researched answer:
-
+  Researched answer: Git is a software use to track changes (think snapshots) to assist teams of developers during software development. Github is a cloud-based way to manage your repositories.
 
 
 2. Which JavaScript operators will return a Boolean value?
 
-  Your answer:
+  Your answer: The JS operators that will return a Boolean value are <, >, =, <=, and >=.
 
-  Researched answer:
+  Researched answer: The following operators will all return a Boolean Value: <, >, ==, ===, <=, and >=.
 
 
 
 3. What is an index? What is the difference between index and value?
 
-  Your answer:
+  Your answer: An index is the actual position something has in an array. Value is the actual value that thing holds.
 
-  Researched answer:
+  Researched answer: The index is the location, while the value is what is stored there.
 
 
 
 4. What is iteration?
 
-  Your answer:
+  Your answer: Iteration is code that will loop until the end of the code
 
-  Researched answer:
+  Researched answer: An iteration is when your code repeats a task until the condition is no longer met. Each time it repeats is called an iteration
 
 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: There is a driver (hands on the keyboard) and a navigator (person who tells the driver "where to go/what to do"). The purpose of pair programming is to speed up the coding process and output cleaner, better code.
 
-  Researched answer:
+  Researched answer: Pair programming is a technique used in agile software development where two developers (driver and navigator) each take turns writing and reviewing code. The driver takes direction from the navigator. The two switch roles often.
 
 
 
@@ -57,12 +56,12 @@ INSTRUCTOR EXAMPLE: What is a conditional statement?
 
 Research and define the following terms to the best of your ability.
 
-1. Higher Order Functions:
+1. Higher Order Functions: A function that either takes two parameters as an argument or returns a function as a result
 
-2. Jest:
+2. Jest: A JS testing framework built by facebook; you can test your JS code with jest
 
-3. Objects:
+3. Objects: Variables that can contain multiple values in the same argument
 
-4. Method:
+4. Method: a property containing a function definition
 
-5. Classes:
+5. Classes: A template for JS objects; Allows you quickly assign objects to new objects
